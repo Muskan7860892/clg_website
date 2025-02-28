d0d0148856a0ccb8f86478fd3a3a67304fd25eb9
muskan <muskan07411@gmail.com>
2025-02-28 12:39:25 +0530

    center

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-
-
 def home():
     return render_template("home.html")
 
@@ -19,6 +17,28 @@ def contact():
 @app.route("/gallery")
 def gallery():
     return render_template("gallery.html")
+
+@app.route('/admission')
+def admission():
+    return render_template('admission.html')
+
+@app.route('/documents')
+def documents():
+    return render_template('documents.html')
+
+@app.route('/sports')
+def sports():
+    return render_template('sports.html')
+
+@app.route('/campus')
+def campus():
+    return render_template('campus.html')  # Renders your campus page
+
+@app.route('/nss')
+def nss():
+    return render_template('nss.html')  # Renders the NSS Events page
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
